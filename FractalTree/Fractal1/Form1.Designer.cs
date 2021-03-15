3b4f7fa6a10b4daec3220100aefe0fa10aabb0e8
Хорунжая Софья <sofiazoom.x99@gmail.com>
2021-03-15 15:41:32 +0300

    message

---
+++ FractalTree/Fractal1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Fractal1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treepanel = new System.Windows.Forms.Panel();
+            this.txtforN = new System.Windows.Forms.TextBox();
+            this.acceptbtn = new System.Windows.Forms.Button();
+            this.labelforN = new System.Windows.Forms.Label();
+            this.checkbranch1 = new System.Windows.Forms.CheckBox();
+            this.checkbranch2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // treepanel
+            // 
+            this.treepanel.Location = new System.Drawing.Point(0, 0);
+            this.treepanel.Name = "treepanel";
+            this.treepanel.Size = new System.Drawing.Size(551, 407);
+            this.treepanel.TabIndex = 0;
+            this.treepanel.Paint += new System.Windows.Forms.PaintEventHandler(this.treepanel_Paint);
+            // 
+            // txtforN
+            // 
+            this.txtforN.Location = new System.Drawing.Point(607, 44);
+            this.txtforN.Name = "txtforN";
+            this.txtforN.Size = new System.Drawing.Size(95, 20);
+            this.txtforN.TabIndex = 1;
+            this.txtforN.Text = "20";
+            // 
+            // acceptbtn
+            // 
+            this.acceptbtn.Location = new System.Drawing.Point(607, 116);
+            this.acceptbtn.Name = "acceptbtn";
+            this.acceptbtn.Size = new System.Drawing.Size(95, 66);
+            this.acceptbtn.TabIndex = 2;
+            this.acceptbtn.Text = "Принять";
+            this.acceptbtn.UseVisualStyleBackColor = true;
+            this.acceptbtn.Click += new System.EventHandler(this.acceptbtn_Click);
+            // 
+            // labelforN
+            // 
+            this.labelforN.AutoSize = true;
+            this.labelforN.Location = new System.Drawing.Point(607, 25);
+            this.labelforN.Name = "labelforN";
+            this.labelforN.Size = new System.Drawing.Size(61, 13);
+            this.labelforN.TabIndex = 3;
+            this.labelforN.Text = "Введите n:";
+            // 
+            // checkbranch1
+            // 
+            this.checkbranch1.AutoSize = true;
+            this.checkbranch1.Location = new System.Drawing.Point(607, 70);
+            this.checkbranch1.Name = "checkbranch1";
+            this.checkbranch1.Size = new System.Drawing.Size(88, 17);
+            this.checkbranch1.TabIndex = 4;
+            this.checkbranch1.Text = "изменение1";
+            this.checkbranch1.UseVisualStyleBackColor = true;
+            // 
+            // checkbranch2
+            // 
+            this.checkbranch2.AutoSize = true;
+            this.checkbranch2.Location = new System.Drawing.Point(607, 93);
+            this.checkbranch2.Name = "checkbranch2";
+            this.checkbranch2.Size = new System.Drawing.Size(88, 17);
+            this.checkbranch2.TabIndex = 5;
+            this.checkbranch2.Text = "изменение2";
+            this.checkbranch2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkbranch2);
+            this.Controls.Add(this.checkbranch1);
+            this.Controls.Add(this.labelforN);
+            this.Controls.Add(this.acceptbtn);
+            this.Controls.Add(this.txtforN);
+            this.Controls.Add(this.treepanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel treepanel;
+        private System.Windows.Forms.TextBox txtforN;
+        private System.Windows.Forms.Button acceptbtn;
+        private System.Windows.Forms.Label labelforN;
+        private System.Windows.Forms.CheckBox checkbranch1;
+        private System.Windows.Forms.CheckBox checkbranch2;
+    }
+}
+
---
+++ FractalTree/Fractal1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Fractal1
+{
+    public partial class Form1 : Form
+    {
+        int n = 20;
+        bool flag1 = true, flag2 = true;
+        public EventHandler evh;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            txtforN.Text = n.ToString();
+            treepanel.Select();
+            txtforN.GotFocus += Focus;
+        }
+        public void DrawF(int x, int y, int len, double angle, PaintEventArgs e, Graphics g=null) //Функция, которая отрисовывает наши ветки, где x и y - координаты, переменная len - длина, а переменная angle - угол поворота
+        {
+            if (g == null)
+            {
+                g = e.Graphics;
+            }
+            double x1, y1; //Новые координаты, которые мы расчитаем исходя из длины и угла
+            x1 = x + len * Math.Sin(angle * Math.PI * 2 / 360.0); // к координате х прибавляем длину умноженную на уравнение окружности и присваиваем это всё в x1
+            y1 = y + len * Math.Cos(angle * Math.PI * 2 / 360.0); // к координате y прибавляем длину умноженную на уравнение окружности и присваиваем это всё в y1
+            g.DrawLine(new Pen(Color.Black, len / 25), x, treepanel.Height - y, (int)x1, treepanel.Height - (int)y1); //Рисуем линию от координат x,y до координат x1,y1; Создаём карандаш Pen(Color.Black, (len/25) - длину делим на 25, чтобы сделать ветки пожирнее), x1 и y1 - преобразуем в целые числа, чтобы осуществлять отрисовку и от panel1.Height вычитаем координаты y и y1, чтобы рисунок отрисовывался с нижней части нашей панели
+            if (len > treepanel.Height / n) // Функция DrawF будет вызываться бесконечно, когда её длина станет больше 20 пикселей, тогда функция DrawF - прекратится
+            {
+                DrawF((int)x1, (int)y1, (int)(len / 1.5), angle + 35, e,g); //Рисуем следующую ветку в координатах x1, y1, длину len делим на 1.5 - чтобы ветки были красивее, а к углу angle - прибавляем 35 градусов, для того чтобы нарисовать правую сторону нашего фрактального древа
+                DrawF((int)x1, (int)y1, (int)(len / 1.5), angle - 35, e,g); //Рисуем следующую ветку в координатах x1, y1, длину len делим на 1.5 - чтобы ветки были красивее, а от угла angle - вычитаем 35 градусов, для того чтобы нарисовать левую сторону нашего фрактального древа
+                if(flag1)
+                    DrawF((int)x1, (int)y1, (int)(len / 1.5), angle + 15, e, g); //Рисуем дополнительные ветки с другими углами, при изменении угла или длины ветви будут изменяться 
+                if(flag2)
+                    DrawF((int)x1, (int)y1, (int)(len / 1.5), angle - 15, e, g); //Рисуем дополнительные ветки с другими углами, при изменинии угла или длины ветви будут изменяться
+            }
+        }
+
+        private void Focus(object sender, EventArgs e)
+        {
+            txtforN.Text = "";
+        }
+
+        private void treepanel_Paint(object sender, PaintEventArgs e)
+        {
+            //UIContext = SynchronizationContext.Current;
+            //if(UIContext!=null)
+            //ThreadPool.QueueUserWorkItem(
+            //    delegate { UIContext.Send(delegate { DrawF(treepanel.Width / 2, 0, (treepanel.Height / 3) / (1 - 2 / 3), 0, e); },null); }
+            //    , null
+            //    );
+            DrawF(treepanel.Width / 2, 0, (treepanel.Height / 3) / (1 - 2 / 3), 0, e); // При помощи функции рисуем в нашей Панеле линию с верха Панели и задаем длину - 300, угол 0, и Paint аргументы передаём 
+
+        }
+
+        private void acceptbtn_Click(object sender, EventArgs e)
+        {
+            n = Convert.ToInt32(txtforN.Text);
+            flag1 = checkbranch1.Checked;
+            flag2 = checkbranch2.Checked;
+            treepanel.Invalidate();
+
+
+        }
+    }
+}
